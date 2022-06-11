cd795d7a298683d545855e6863ec71377309d87e
“Edu58” <edumuriithi58@gmail.com>
2022-06-11 10:41:42 +0300

    adds test_profile_creation

--- main/tests.py
+++ main/tests.py
@@ -7,8 +7,16 @@ from .models import Profile, Project, Rating
 class TestProfile(TestCase):
     def setUp(self):
         self.john = User(username="john", first_name="john", last_name="bravo", email="john@gmail.com", password="john98765")
-        self.john.save()
 
     def test_user_instance(self):
+        self.john.save()
         self.assertTrue(isinstance(self.john, User))
         
+    def test_profile_creation(self):
+        self.john.save()
+        self.john_profile = Profile(user=self.john ,bio="I am johny bravo", portfolio="https://johnthedev.com", github="https://github.com/john")
+        self.john_profile.save()
+
+        self.assertTrue(self.john.profile.bio, "I am johny bravo")
+        self.assertTrue(self.john.profile.portfolio, "https://johnthedev.com")
+        self.assertTrue(self.john.profile.github, "https://github.com/john")
