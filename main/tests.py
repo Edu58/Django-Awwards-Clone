5e4f5c15743363c5944f5a50722c33c2cf0a1613
“Edu58” <edumuriithi58@gmail.com>
2022-06-11 11:01:00 +0300

    adds test_project_instance

--- main/tests.py
+++ main/tests.py
@@ -32,4 +32,24 @@ class TestProfile(TestCase):
         self.assertTrue(self.john.profile.github, "https://github.com/john")
 
 
+class TestProject(TestCase):
+    def setUp(self):
+        self.john = User(username="john", first_name="john", last_name="bravo", email="john@gmail.com", password="john98765")
+        self.john.save()
+        self.john_profile = Profile(user=self.john ,bio="I am johny bravo", portfolio="https://johnthedev.com", github="https://github.com/john")
+        self.john_profile.save()
 
+    def tearDown(self):
+        self.john.delete()
+
+    def test_project_instance(self):
+        self.new_project = Project(
+            title="Awaards Clone", 
+            landing_page="./static/images/default-website-landing-page.png", 
+            description="This is a clone for the famous Awaards website",
+            link = "https://awaards-clone.co.ke",
+            user = self.john
+            )
+
+        self.assertTrue(self.new_project, Project)
+        
