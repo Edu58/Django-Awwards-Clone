5d12f5220b135e89d5a93df759fa8ef4ea005811
“Edu58” <edumuriithi58@gmail.com>
2022-06-11 10:37:15 +0300

    adds test_user_instance

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile, Project, Rating
+
 
 # Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.john = User(username="john", first_name="john", last_name="bravo", email="john@gmail.com", password="john98765")
+        self.john.save()
+
+    def test_user_instance(self):
+        self.assertTrue(isinstance(self.john, User))
+        
