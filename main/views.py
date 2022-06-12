d3b8b7b4c6746510ded6d5f8f644729ce950d934
“Edu58” <edumuriithi58@gmail.com>
2022-06-12 16:45:10 +0300

    adds LoginUserForm import to views

--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .forms import RateProjectForm, SubmitProjectForm
 from .models import Profile, Project, Rating
 from django.urls import reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import LoginUserForm
 
 
 def login_user(request):
