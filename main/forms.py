70ca438420d85aaad9fd305f2f8d0fa656b563eb
“Edu58” <edumuriithi58@gmail.com>
2022-06-12 16:44:03 +0300

    adds LoginUserForm

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,15 @@
 from django import forms
 from .models import Project, Rating
 from ckeditor.widgets import CKEditorWidget
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class LoginUserForm(UserCreationForm):
+    username = forms.CharField(required=True)
+    class Meta:
+        model = User
+        fields = ['username', 'password1']
 
 
 class SubmitProjectForm(forms.ModelForm):
