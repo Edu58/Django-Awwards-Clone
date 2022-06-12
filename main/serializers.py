df4afe7b05396dd4d9cbe279ec3b9e790ce44a8c
“Edu58” <edumuriithi58@gmail.com>
2022-06-12 22:48:38 +0300

    adds ProfilesSerializer class

---
+++ main/serializers.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import Profile, Project
+
+class ProfilesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
