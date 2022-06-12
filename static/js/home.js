85fa3a85564d5f7aad21fa689a1d6cf9096b8f64
“Edu58” <edumuriithi58@gmail.com>
2022-06-12 14:12:48 +0300

    style: adds search bar to home page

--- static/js/home.js
+++ static/js/home.js
@@ -1,14 +1,10 @@
-$('document').ready(function () {
-     $('.details').hide()
-
-    $('.card').on('mouseover', function () {
-        console.log('asfasdf')
-        $(this).children('.project-image').addClass('blur')
-        $(this).children('.details').fadeIn("fast")
-    })
-
-    $('.card').on('mouseleave', function () {
-        $(this).children('.project-image').removeClass('blur')
-        $(this).children('.details').fadeOut("fast")
-    })
+$('.details').hide()
+$('.card').on('mouseover', function () {
+    console.log('asfasdf')
+    $(this).children('.project-image').addClass('blur')
+    $(this).children('.details').fadeIn("fast")
+})
+$('.card').on('mouseleave', function () {
+    $(this).children('.project-image').removeClass('blur')
+    $(this).children('.details').fadeOut("fast")
 })
